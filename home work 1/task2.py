4900e891826dc5011f4a3b7a35adbf67cb2eb874
Dmitriy Begishev <mintdragon@yandex.ru>
2022-12-30 19:18:59 +0400

    second task complete

--- home work 1/task2.py
+++ home work 1/task2.py
@@ -1,7 +1,3 @@
-# https://university.ylab.site/python/lecture-1-hw/#zadacha-no2.
-# https://service-calc.ru/raznoe/sistemy-schisleniya-perevod#i-7
-# https://calculatori.ru/perevod-chisel.html
-
 """
 Задача №2.
 Написать метод int32_to_ip, который принимает на вход 32-битное целое 
@@ -9,16 +5,68 @@
 """
 
 def int32_to_ip(int32: int) -> str:
-  binary_value: int = 0
-  
-  def convert_to_binary(decimal_value: int) -> int:
-    division_value: int = decimal_value
-    binary_value: str = ''
+    if int32 > 4294967295:
+        return "The value is too high!! Try to choose a smaller value."
+
+    def convert_to_binary(decimal_value: int) -> str:
+        if decimal_value == 0:
+            return '0'
     
-    while division_value != 0:
-      binary_value += ''
+        division_result: int = decimal_value
+        reverse_binary_value: str = ''
+    
+        while division_result != 0:
+            reverse_binary_value += str(division_result % 2)
+            division_result = division_result // 2
+
+        binary_value: str = reverse_binary_value[::-1]
+        return binary_value
 
 
-#assert int32_to_ip(2154959208) == "128.114.17.104"
-#assert int32_to_ip(0) == "0.0.0.0"
-#assert int32_to_ip(2149583361) == "128.32.10.1"
+    def split_into_octets(binary_value: str) -> str:
+        reverse_binary_value: str = binary_value[::-1]
+
+        i: int = 0
+        reverse_list_of_octets: list = []
+        while i < len(reverse_binary_value):
+            octet:str = reverse_binary_value[i:i+8]
+            if len(octet) < 8:
+                octet = octet + (8 - len(octet)) * '0'
+            reverse_list_of_octets.append(octet[::-1])
+            i += 8
+
+        while len(reverse_list_of_octets) < 4:
+            reverse_list_of_octets.append('00000000')
+
+        list_of_octets: list = reverse_list_of_octets[::-1]
+        value_devided_into_octets: str = '.'.join(list_of_octets)
+        return value_devided_into_octets
+
+    def convert_octet_to_decimal(double_octets:str) -> str:
+        list_of_double_octets: list = double_octets.split('.')
+        list_of_decimal_octets: list = []
+
+        for double_octet in list_of_double_octets:
+            decimal_octet = 0
+            reverce_double_octet: str = double_octet[::-1]
+
+            i:int = 0
+            while i < len(reverce_double_octet):
+                decimal_octet += int(reverce_double_octet[i]) * 2 ** i
+                i += 1
+            
+            list_of_decimal_octets.append(str(decimal_octet))
+
+        value_of_decimal_octets: str = '.'.join(list_of_decimal_octets)
+        return value_of_decimal_octets
+    
+
+    binary_value: str = convert_to_binary(int32)
+    value_split_into_octets: str = split_into_octets(binary_value)
+    decimal_value_split_into_octets: str = convert_octet_to_decimal(value_split_into_octets)
+    return decimal_value_split_into_octets
+
+
+assert int32_to_ip(2154959208) == "128.114.17.104"
+assert int32_to_ip(0) == "0.0.0.0"
+assert int32_to_ip(2149583361) == "128.32.10.1"
