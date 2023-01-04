19cdfa2bc5c1b31017014085b6590eda392d187d
Dmitriy Begishev <mintdragon@yandex.ru>
2023-01-04 18:46:13 +0400

    added recursion, but it did not work! Added correct solution

--- home work 1/task4.py
+++ home work 1/task4.py
@@ -3,55 +3,55 @@
 Написать метод bananas, который принимает на вход строку и возвращает  
 количество слов 'banana' в строке.
 """
+import copy
 
 def bananas(s: str) -> set:
     result = set()
-    control_word: str = 'banana'
-    buffer :str = ''
-    len_s: int =  len(s)
-    len_conrtol_word: int = len(control_word)
+    word = 'banana'
 
-    print(s)
-
-    if len_s < len_conrtol_word:
+    if len(s) < len(word):
         return result
 
-    def ola(s, control_word, buffer):
-        i: int = 0 # счетчик для контрольного слова
-        len_s = len(s)
-        len_conrtol_word = len(control_word)
+    def recursion(buffer: str, s_index: int, word_index: int, i) -> set:
+        #print(f'{i}-> s_index: {s_index}   word_index: {word_index}')
+        #if len(s) < word_index:
+        if buffer.replace('-', '') == word:
+            #print(f'len_s: {len(s)}, word_index: {word_index}')
+            result.add(buffer + '-' * (len(s) - len(buffer)))
+        i += 1
+        if len(s) > s_index :
+            for j in range(s_index, len(s)):
+                print(f's_index: {s_index}, word_index: {word_index}')
+                if word_index < len(word) and s[s_index] == word[word_index]: 
+                    buffer += s[s_index]
+                    s_index += 1
+                    word_index += 1
+                else:
+                    s_index += 1
+                recursion('-' * (s_index - len(buffer)) + buffer, 
+                        s_index, 
+                        word_index, 
+                        i)
 
-        for j in range(len_s):
-            print(f'len_s: {len_s}, len_conrtol_word: {len_conrtol_word}')
-            if len_s < len_conrtol_word:
-                break
-            if s[i] == control_word[0]:
-                buffer += s[0]
-                s = s[1:]
-                control_word = control_word[1:]
-                ola(s, control_word, buffer)
-            else:
-                buffer += '-'
-                i += 1
-            print(f"i: {i}, j: {j}, buffer: {buffer}")
-            if i >= len_s:
-                break
-            print()
-        return buffer
-
-    buffer = ola(s, control_word, buffer)
+        return result
+    st = set()
+    def f(ss, m, n):
+        if n > 5:
+            st.add(ss + '-'*(len(s)-m))
+        elif m < len(s):
+            for i in range(m, len(s)):
+                if s[i] == 'banana'[n]:
+                    f(ss+'-'*(i-m)+s[i], i+1, n+1)
+        return
+    f('', 0, 0)
+    return st  
 
 
-    if buffer.replace('-', '') == control_word:
-        result.add(buffer)
-
-
-    return result
-
+    #return recursion(buffer='', s_index=0, word_index=0, i = 0)
 
 #print("banann: -> " + str(bananas("banann")), end='\n\n')
-print("banana: -> " + str(bananas("banana")), end='\n\n')
-#print("bbananana: -> " + str(bananas("bbananana")), end='\n\n')
+#print("banana: -> " + str(bananas("banana")), end='\n\n')
+print("bbananana: -> " + str(bananas("bbananana")), end='\n\n')
 
 '''assert bananas("banann") == set()
 assert bananas("banana") == {"banana"}
